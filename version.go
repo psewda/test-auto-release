a6e2f101fbe74548b653131aaac1e996f8b7984c
Prakash Sewda <psewdablr@gmail.com>
2021-02-18 10:47:06 +0000

    change

---
+++ version.go
@@ -0,0 +1,10 @@
+package typing
+
+// Version is the place holder to put version
+// string. It is set dynamically in the ci/cd pipeline.
+var Version = "1.0.0"
+
+// BuildNumber is the place holder to put build-number
+// string. It is set dynamically in the ci/cd pipeline.
+var BuildNumber = "1"
+
