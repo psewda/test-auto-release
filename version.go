8e5353cb222c2fb15efbae2c9b68ff03e065a952
Travis CI User <travis@example.org>
2021-02-22 17:49:45 +0000

    ci: bump version to: 4.0.6 [skip ci]

--- version.go
+++ version.go
@@ -2,7 +2,7 @@ package autorelease
 
 // Version is the place holder to put version
 // string. It is set dynamically in the ci/cd pipeline.
-var Version = "4.0.5"
+var Version = "4.0.6"
 
 // BuildNumber is the place holder to put build-number
 // string. It is set dynamically in the ci/cd pipeline.
